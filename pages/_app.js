69be6a4b42caf68df93866f6856082e65b6369aa
Faiz Ahmed <4faizahmed@gmail.com>
2022-09-12 09:13:12 +0300

    Project configuration

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import {useEffect} from "react";
+import Head from "next/head";
+import Navbar from "../components/Navbar";
+import '../styles/globals.scss';
+import "bootstrap/dist/css/bootstrap.min.css"
+import Footer from "../components/Footer";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}) {
+
+  useEffect(() => {
+    window.bootstrap = require('bootstrap');
+  }, [])
+
+  return (
+      <>
+        <Head>
+          <title>Apple</title>
+          <meta charSet="utf-8"/>
+          <meta name="viewport" content="width=device-width, initial-scale=1"/>
+          <meta name="format-detection" content="telephone=no"/>
+          <meta name="description" content="Your one stop shop for all your apple products and the best prices"/>
+          <meta name="keywords" content="apple, ecommerce, best, deals, kenya"/>
+        </Head>
+        <Navbar/>
+        <Component {...pageProps} />
+        <Footer/>
+      </>
+  )
 }
 
 export default MyApp
