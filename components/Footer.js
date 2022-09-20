2870531379590ac028dd5ff432aa08fa20091cb3
Farouk <thiagoacookie@gmail.com>
2022-09-20 11:49:33 +0300

    Categories conflict fix

--- components/Footer.js
+++ components/Footer.js
@@ -7,57 +7,57 @@ import {faEnvelope} from "@fortawesome/free-solid-svg-icons";
 function Footer() {
     return (
         <div className='container-fluid footer'>
-        <div className='container'>
-            <div className='footer-top row'>
-             <div className='products-section col-3'>
-                 <h1>Products</h1>
-                  <ul>
-                      <li>Macbook</li>
-                      <li>Ipad</li>
-                      <li>Iphone</li>
-                      <li>Accessories</li>
-                  </ul>
-             </div>
-             <div className='customer-service col-3'>
-                 <h1>Customer Service</h1>
-                 <ul>
-                     <li>Order Tracking</li>
-                     <li>About Us</li>
-                     <li>Careers</li>
-                     <li>Contacts</li>
-                 </ul>
-             </div>
-             <div className='socials-section col-3'>
-                 <h1>Socials</h1>
-                 <ul>
-                     <li><a href="#">Twitter</a></li>
-                     <li><a href="#">Instagram</a></li>
-                     <li><a href="#">Facebook</a></li>
-                     <li><a href="#">Linked In</a></li>
-                 </ul>
-             </div>
-             <div className='contacts col-3'>
-                 <h1>Contacts</h1>
-                 <ul>
-                         <li>Apple Express Ltd</li>
-                         <li><FontAwesomeIcon icon={faPhone} aria-hidden="phone" className="phone"></FontAwesomeIcon>  +254 723 456 789</li>
-                         <li><FontAwesomeIcon icon={faLocation} aria-hidden='location' className='arrow-location'/> Nairobi, Kenya</li>
-                         <li><FontAwesomeIcon icon={faEnvelope} aria-hidden='enveloper' className='envelope'/> info@appleexpress.co.ke</li>
-                 </ul>
-             </div>
-        </div>
-            <hr/>
-            <div className='footer-bottom row'>
-                <p className='col-md-12 col-lg-5'>Apple Express &copy; 2022 </p>
-                <div className='d-flex col-md-12 col-lg-7'>
-                    <p className='me-5 p-border-right'>Terms & Conditions</p>
-                    <p className='me-5 p-border-right'>Returns & Refunds Policy</p>
-                    <p className='me-5 p-border-right'>Privacy Policy</p>
-                    <p className='me-5 p-border-right'>Shipping</p>
-                    <p className='me-5'>Faqs</p>
+            <div className='container'>
+                <div className='footer-top row'>
+                    <div className='products-section col-3'>
+                        <h1>Products</h1>
+                        <ul>
+                            <li>Macbook</li>
+                            <li>Ipad</li>
+                            <li>Iphone</li>
+                            <li>Accessories</li>
+                        </ul>
+                    </div>
+                    <div className='customer-service col-3'>
+                        <h1>Customer Service</h1>
+                        <ul>
+                            <li>Order Tracking</li>
+                            <li>About Us</li>
+                            <li>Careers</li>
+                            <li>Contacts</li>
+                        </ul>
+                    </div>
+                    <div className='socials-section col-3'>
+                        <h1>Socials</h1>
+                        <ul>
+                            <li><a href="#">Twitter</a></li>
+                            <li><a href="#">Instagram</a></li>
+                            <li><a href="#">Facebook</a></li>
+                            <li><a href="#">Linked In</a></li>
+                        </ul>
+                    </div>
+                    <div className='contacts col-3'>
+                        <h1>Contacts</h1>
+                        <ul>
+                            <li>Apple Express Ltd</li>
+                            <li><FontAwesomeIcon icon={faPhone} aria-hidden="false" className="phone"></FontAwesomeIcon>  +254 723 456 789</li>
+                            <li><FontAwesomeIcon icon={faLocation} aria-hidden='false' className='arrow-location'/> Nairobi, Kenya</li>
+                            <li><FontAwesomeIcon icon={faEnvelope} aria-hidden='false' className='envelope'/> info@appleexpress.co.ke</li>
+                        </ul>
+                    </div>
+                </div>
+                <hr/>
+                <div className='footer-bottom row'>
+                    <p className='col-md-12 col-lg-5'>Apple Express &copy; 2022 </p>
+                    <div className='d-flex col-md-12 col-lg-7'>
+                        <p className='me-5 p-border-right'>Terms & Conditions</p>
+                        <p className='me-5 p-border-right'>Returns & Refunds Policy</p>
+                        <p className='me-5 p-border-right'>Privacy Policy</p>
+                        <p className='me-5 p-border-right'>Shipping</p>
+                        <p className='me-5'>Faqs</p>
+                    </div>
                 </div>
             </div>
-        </div>
         </div>
     )
 }
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,16 +1,17 @@
 import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
-import {faBagShopping,faSearch} from "@fortawesome/free-solid-svg-icons";
+import {faBagShopping} from "@fortawesome/free-solid-svg-icons";
 import {faMagnifyingGlass} from "@fortawesome/free-solid-svg-icons";
+import Image from "next/image";
 
 function Navbar() {
     return (
-        <div >
-            <nav className="navbar navbar-expand-lg bg-dark">
+
+        <div>
+            <nav className="navbar navbar-expand-lg">
                 <div className="container-fluid">
-                    <a className="navbar-brand text-white-50 header-name ms-2" href="#">
-                        <img src="images/favicon/apple-touch-icon.png" className="logo"/>
-                        express
-                    </a>
+
+                    <Image src="/images/favicon/apple-touch-icon.png" alt="favicon-apple" width="20px" height="25px" className="logo"/>
+
                     <button className="navbar-toggler navbar-dark" type="button" data-bs-toggle="collapse"
                             data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
                             aria-expanded="false" aria-label="Toggle navigation">
@@ -22,7 +23,7 @@ function Navbar() {
                                 <a className="nav-link me-2 links" aria-current="page" href="#">Store</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link  me-2 links" href="#">Mac</a>
+                                <a className="nav-link  me-2 links " href="#">Mac</a>
                             </li>
                             <li className="nav-item">
                                 <a className="nav-link me-2 links" href="#">Ipad</a>
@@ -50,11 +51,11 @@ function Navbar() {
                             </li>
                         </ul>
                         <form className="d-flex input-icon me-3" role="search">
-                            <FontAwesomeIcon icon={faMagnifyingGlass} aria-hidden='search icon' className='search'/>
+                            <FontAwesomeIcon icon={faMagnifyingGlass} aria-hidden={false} className='search'/>
                             <input className="form-control me-2 input-field" type="search" placeholder="Search" aria-label="Search"/>
                         </form>
                         <div className='cart'>
-                            <FontAwesomeIcon icon={faBagShopping} aria-hidden="shopping bag" className="bag" />
+                            <FontAwesomeIcon icon={faBagShopping} aria-hidden={false} className="bag" />
                         </div>
                     </div>
                 </div>
