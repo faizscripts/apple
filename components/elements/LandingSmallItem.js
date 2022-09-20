27eaaccf2437262973f79ed5eed60e883e884c80
Tevin Aggrey <taggrey26@gmail.com>
2022-09-20 15:04:46 +0300

    Duplicate code removal

--- components/elements/LandingSmallItem.js
+++ components/elements/LandingSmallItem.js
@@ -8,19 +8,17 @@ function LandingSmallItem({title,subhead,image,textColor}) {
 
     return (
 
-            <div className="col-md-6">
-                <div className="landing-section-small">
+            <div className="col-lg-6">
+                <div className="landing-section small">
                     <div className="backgroundImg"
                          style={{backgroundImage: `url('${image}')`}}>
                     </div>
 
                     <div className={`top-title ${textColor}`}>
-                        <h4 className="headline"> <Image src="/images/favicon/favicon-sm.png" alt="" width="30px" height="30px"/> {title}</h4>
+                        <h4 className="headline"> <Image src="/images/favicon/favicon-md.png" alt="" width="25px" height="30px"/> {title}</h4>
                         <h5 className="subhead" role="presentation">{subhead}</h5>
                         <div className="cta-links">
-                            <a href="https://www.apple.com/iphone-14-pro/" aria-label="Learn more about iphone 14 pro">learn more <Image
-                                src="/images/landing/right-arrow-blue.svg" alt="" width="12px" height="12px" /></a>
-                            <a href="https://www.apple.com/iphone-14-pro/" aria-label="Learn more about iphone 14 pro">Pre-order <Image
+                            <a href="https://www.apple.com/iphone-14-pro/" aria-label="Learn more about iphone 14 pro" className="links-to-buy">Buy <Image
                                 src="/images/landing/right-arrow-blue.svg" alt="" width="12px" height="12px"/></a>
                         </div>
                     </div>
